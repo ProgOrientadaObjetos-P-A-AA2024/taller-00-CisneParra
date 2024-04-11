571e62454cdbf152223a5272f4907788402f679a
Antonella Parra <adparra2@gmail.com>
2024-04-11 17:17:28 -0500

    Cambio de la clase

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -23,10 +23,15 @@ public class Demo002 {
         String mensaje = Mensaje.obtenerMensaje();
         int habitantes = Informacion.obtenerHabitantes();
         String cadena = String.format("%scon %d\n", mensaje, habitantes);
+        // Despues de llamar a las funciones procedo a guardarlas en una cadena
+        // acumuladora para poder imprimir de mejor manera
+        
         int r;
         System.out.println("Digite del 1 al 5 si desea el mensaje original");
         r = entrada.nextInt();
+        // Pregunte al usuario para ingresar por teclado algun valor
         if (r <= 5 && r > 0) {
+            // comparo las posibles respuestas del usuario
             System.out.printf("%s", cadena);
         } else {
             System.out.printf("%s", cadena.toUpperCase());
